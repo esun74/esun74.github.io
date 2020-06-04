5fe0d401282e3b19e69742a8424040083ae70fb0
Eric Sun <ericsun19591827346@gmail.com>
2020-06-04 01:59:05 -0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -83,7 +83,7 @@ scene.add(objects)
 var concrete_material = new THREE.MeshLambertMaterial({
 		color: 0xBBBBBB,
 })
-var polygon = new THREE.Mesh(new THREE.BoxBufferGeometry(0.1, 25, 25), concrete_material)
+var polygon = new THREE.Mesh(new THREE.BoxBufferGeometry(0.1, 50, 50), concrete_material)
 polygon.position.set(-0.50, 0, 0)
 polygon.receiveShadow = true
 objects.add(polygon)
@@ -97,7 +97,7 @@ var ring_material = new THREE.MeshLambertMaterial({
 
 var ring_geometry = new THREE.Geometry()
 for (var i = 0; i < 8; i++) {
-	ring_geometry.merge(new THREE.RingGeometry(4.5, 5.0, 8, 1, (Math.PI / 4) * i, (Math.PI / 4) - 0.05))
+	ring_geometry.merge(new THREE.RingGeometry(4.0, 4.5, 8, 1, (Math.PI / 4) * i + 0.025, (Math.PI / 4) - 0.05))
 }
 
 
@@ -114,8 +114,8 @@ var loop_material = new THREE.MeshLambertMaterial({
 		color: 0x6D6D6D,
 })
 var loop_geometry = new THREE.Geometry()
-loop_geometry.merge(new THREE.TorusGeometry(3.0, 0.015, 6, 64, Math.PI * 1.95))
-loop_geometry.rotateZ(Math.PI / 2 + 0.06)
+loop_geometry.merge(new THREE.TorusGeometry(3.0, 0.015, 6, 64, Math.PI * 1.96))
+loop_geometry.rotateZ(Math.PI / 2 + 0.04)
 
 var pole_locations = [0, 1/6, 2.5/6, 3.5/6, 5/6, 6/6, 7/6, 8.5/6, 9.5/6, 11/6]
 
@@ -129,98 +129,146 @@ for (var i = 0; i < pole_locations.length; i++) {
 	loop_geometry.merge(pole_geometry)
 }
 
-
-
 var polygon = new THREE.Mesh(new THREE.BufferGeometry().fromGeometry(loop_geometry), loop_material)
 polygon.position.set(0, 0, 0)
-polygon.rotation.set(0, Math.PI / 2, 0)
+polygon.rotation.set(0, Math.PI / 2, 0.025)
 polygon.castShadow = true
 polygon.receiveShadow = true
 objects.add(polygon)
 //--------------------------------------------------
 
 
+// Markers
+//--------------------------------------------------
+var marker_material = new THREE.MeshLambertMaterial({
+		color: 0xFFFFFF,
+})
+
+var marker_geometry = new THREE.Geometry()
+
+var pole_geometry = new THREE.CylinderGeometry(0.05, 0.05, 1.0, 6, 1, false)
+pole_geometry.translate(0, 0.25, 0)
+pole_geometry.rotateZ(Math.PI / 2)
+marker_geometry.merge(pole_geometry)
 
 
-var material = new THREE.MeshLambertMaterial({
+var mark_locations = [1/6, 2/6, 4/6, 5/6, 7/6, 8/6, 10/6, 11/6]
+for (var i = 0; i < mark_locations.length; i++) {
+	var mark_geometry = new THREE.BoxGeometry(0.025, 0.4, 0.12)
+	mark_geometry.translate(0.075, 3.125, 0)
+	mark_geometry.rotateX(Math.PI * mark_locations[i])
+	marker_geometry.merge(mark_geometry)
+
+	var mark_geometry = new THREE.BoxGeometry(0.025, 0.1, 0.1)
+	mark_geometry.translate(-0.45, 1.7, 0)
+	mark_geometry.rotateX(Math.PI * mark_locations[i])
+	marker_geometry.merge(mark_geometry)
+}
+
+
+var mark_locations = [3/6, 6/6, 9/6]
+for (var i = 0; i < mark_locations.length; i++) {
+	var mark_geometry = new THREE.BoxGeometry(0.025, 0.6, 0.15)
+	mark_geometry.translate(0.075, 3.2, 0)
+	mark_geometry.rotateX(Math.PI * mark_locations[i])
+	marker_geometry.merge(mark_geometry)
+
+	var mark_geometry = new THREE.BoxGeometry(0.025, 0.1, 0.1)
+	mark_geometry.translate(-0.45, 1.7, 0)
+	mark_geometry.rotateX(Math.PI * mark_locations[i])
+	marker_geometry.merge(mark_geometry)
+}
+
+
+var mark_locations = [0/6]
+for (var i = 0; i < mark_locations.length; i++) {
+	var mark_geometry = new THREE.BoxGeometry(0.025, 1.2, 0.15)
+	mark_geometry.translate(0.000, 3.1, -0.12)
+	mark_geometry.rotateX(Math.PI * mark_locations[i])
+	marker_geometry.merge(mark_geometry)
+
+	var mark_geometry = new THREE.BoxGeometry(0.025, 1.2, 0.15)
+	mark_geometry.translate(0.000, 3.1, +0.12)
+	mark_geometry.rotateX(Math.PI * mark_locations[i])
+	marker_geometry.merge(mark_geometry)
+
+	var mark_geometry = new THREE.BoxGeometry(0.025, 0.12, 0.12)
+	mark_geometry.translate(-0.45, 1.7, -0.1)
+	mark_geometry.rotateX(Math.PI * mark_locations[i])
+	marker_geometry.merge(mark_geometry)
+
+	var mark_geometry = new THREE.BoxGeometry(0.025, 0.12, 0.12)
+	mark_geometry.translate(-0.45, 1.7, +0.1)
+	mark_geometry.rotateX(Math.PI * mark_locations[i])
+	marker_geometry.merge(mark_geometry)
+}
+
+
+var polygon = new THREE.Mesh(new THREE.BufferGeometry().fromGeometry(marker_geometry), marker_material)
+// polygon.position.set(0, 0, 0)
+// polygon.rotation.set(0, Math.PI / 2, 0)
+polygon.castShadow = true
+polygon.receiveShadow = true
+objects.add(polygon)
+
+//--------------------------------------------------
+
+
+
+// Hand
+//--------------------------------------------------
+
+var marker_material = new THREE.MeshLambertMaterial({
 		color: 0xCCCCCC,
 })
 
-// Center Box
-//--------------------------------------------------
-var polygon = new THREE.Mesh(new THREE.BoxBufferGeometry(Math.random() * 0.5 + 0.2, Math.random() + 1.0, Math.random() * 0.5 + 0.2), material)
-polygon.position.set(0, -2.5, 0)
-polygon.castShadow = true
-polygon.receiveShadow = true
+var marker_geometry = new THREE.Geometry()
+
+var pole_geometry = new THREE.CylinderGeometry(0.125, 0.125, 0.01, 6, 1, false)
+pole_geometry.translate(0, -0.195, 0)
+pole_geometry.rotateZ(Math.PI / 2)
+pole_geometry.rotateX(Math.PI / 2)
+marker_geometry.merge(pole_geometry)
+
+var polygon = new THREE.Mesh(marker_geometry, marker_material)
 objects.add(polygon)
+
+
 //--------------------------------------------------
 
 
 
-// Hexagons
+// Grid
 //--------------------------------------------------
-var count = 3
-var geometries = new THREE.BufferGeometry()
-var geo_positions = new Float32Array(count * 162)
-
-for (var i = 0; i < count; i++) {
-	var x = (i * 2.5) - 2.5
-	var y = -3.0
-	var z = 2.5
-	var radius = Math.random() * 0.15 + 0.25
-	var height = Math.random() + 1.0
-
-	for (var side = 0; side < 6; side++) {
-		var angle_1 = Math.PI * (side + 0) / 3
-		var angle_2 = Math.PI * (side + 1) / 3
-
-		geo_positions[(162 * i) + (27 * side) + 0] = x + (radius * Math.cos(angle_1))
-		geo_positions[(162 * i) + (27 * side) + 1] = y + height
-		geo_positions[(162 * i) + (27 * side) + 2] = z + (radius * Math.sin(angle_1))
-
-		geo_positions[(162 * i) + (27 * side) + 3] = x + (radius * Math.cos(angle_2))
-		geo_positions[(162 * i) + (27 * side) + 4] = y + height
-		geo_positions[(162 * i) + (27 * side) + 5] = z + (radius * Math.sin(angle_2))
-
-		geo_positions[(162 * i) + (27 * side) + 6] = x + (radius * Math.cos(angle_2))
-		geo_positions[(162 * i) + (27 * side) + 7] = y
-		geo_positions[(162 * i) + (27 * side) + 8] = z + (radius * Math.sin(angle_2))
 
 
-		geo_positions[(162 * i) + (27 * side) + 9] = x + (radius * Math.cos(angle_1))
-		geo_positions[(162 * i) + (27 * side) + 10] = y + height
-		geo_positions[(162 * i) + (27 * side) + 11] = z + (radius * Math.sin(angle_1))
+var grid_material = new THREE.MeshLambertMaterial({
+		color: 0x777777,
+})
 
-		geo_positions[(162 * i) + (27 * side) + 12] = x + (radius * Math.cos(angle_2))
-		geo_positions[(162 * i) + (27 * side) + 13] = y
-		geo_positions[(162 * i) + (27 * side) + 14] = z + (radius * Math.sin(angle_2))
+var polygon = new THREE.Mesh(new THREE.BoxBufferGeometry(.01, 18, .01), grid_material)
+polygon.position.set(-0.35, 0, +5.25)
+objects.add(polygon)
 
-		geo_positions[(162 * i) + (27 * side) + 15] = x + (radius * Math.cos(angle_1))
-		geo_positions[(162 * i) + (27 * side) + 16] = y
-		geo_positions[(162 * i) + (27 * side) + 17] = z + (radius * Math.sin(angle_1))
+var polygon = new THREE.Mesh(new THREE.BoxBufferGeometry(.01, 18, .01), grid_material)
+polygon.position.set(-0.35, 0, -5.25)
+objects.add(polygon)
 
+var polygon = new THREE.Mesh(new THREE.BoxBufferGeometry(.01, .01, 18), grid_material)
+polygon.position.set(-0.35, +1.75, 0)
+objects.add(polygon)
 
-		geo_positions[(162 * i) + (27 * side) + 18] = x + (radius * Math.cos(angle_1))
-		geo_positions[(162 * i) + (27 * side) + 19] = y + height
-		geo_positions[(162 * i) + (27 * side) + 20] = z + (radius * Math.sin(angle_1))
+var polygon = new THREE.Mesh(new THREE.BoxBufferGeometry(.01, .01, 18), grid_material)
+polygon.position.set(-0.35, +5.25, 0)
+objects.add(polygon)
 
-		geo_positions[(162 * i) + (27 * side) + 21] = x
-		geo_positions[(162 * i) + (27 * side) + 22] = y + height
-		geo_positions[(162 * i) + (27 * side) + 23] = z
+var polygon = new THREE.Mesh(new THREE.BoxBufferGeometry(.01, .01, 18), grid_material)
+polygon.position.set(-0.35, -1.75, 0)
+objects.add(polygon)
 
-		geo_positions[(162 * i) + (27 * side) + 24] = x + (radius * Math.cos(angle_2))
-		geo_positions[(162 * i) + (27 * side) + 25] = y + height
-		geo_positions[(162 * i) + (27 * side) + 26] = z + (radius * Math.sin(angle_2))
-	}
-}
-geometries.addAttribute('position', new THREE.BufferAttribute(geo_positions, 3))
-geometries.computeVertexNormals()
-
-var hexagons = new THREE.Mesh(geometries, material)
-hexagons.castShadow = true
-hexagons.receiveShadow = true
-
-objects.add(hexagons)
+var polygon = new THREE.Mesh(new THREE.BoxBufferGeometry(.01, .01, 18), grid_material)
+polygon.position.set(-0.35, -5.25, 0)
+objects.add(polygon)
 //--------------------------------------------------
 
 
@@ -260,9 +308,11 @@ var directional_light = new THREE.DirectionalLight(0xFFFFFF, 0.75)
 directional_light.castShadow = true
 directional_light.shadow.mapSize.width = 2048
 directional_light.shadow.mapSize.height = 2048
+directional_light.shadow.mapSize.width = 4096
+directional_light.shadow.mapSize.height = 4096
 directional_light.shadow.camera.near = sun_distance - 100
 directional_light.shadow.camera.far = sun_distance + 100
-directional_light.shadow.bias = - 0.00001;
+directional_light.shadow.bias = - 0.0001;
 
 objects.add(directional_light)
 
@@ -272,19 +322,7 @@ objects.add(ambient_light)
 
 
 
-// Set Sun Position
-//--------------------------------------------------
-var theta = -Math.PI * ((inclination))
-var phi = 2 * Math.PI * azimuth;
 
-directional_light.position.x = sun_distance * Math.cos(phi);
-directional_light.position.y = sun_distance * Math.sin(phi) * Math.sin(theta);
-directional_light.position.z = sun_distance * Math.sin(phi) * Math.cos(theta);
-
-directional_light.intensity = Math.min(directional_light.position.y / 100000, 1.0)
-ambient_light.intensity = directional_light.intensity / 3
-sky.material.uniforms["sunPosition"].value.copy(directional_light.position)
-//--------------------------------------------------
 
 
 
@@ -295,6 +333,24 @@ var animate = function () {
 	requestAnimationFrame(animate)
 	raycaster.setFromCamera(mouse, camera)
 
+
+	// Set Sun Position
+	//--------------------------------------------------
+	inclination = (inclination % 1 + 1) + 0.001
+
+	console.log(inclination)
+
+	var theta = -Math.PI * ((inclination))
+	var phi = 2 * Math.PI * azimuth;
+
+	directional_light.position.x = sun_distance * Math.cos(phi);
+	directional_light.position.y = sun_distance * Math.sin(phi) * Math.sin(theta);
+	directional_light.position.z = sun_distance * Math.sin(phi) * Math.cos(theta);
+
+	directional_light.intensity = Math.min(directional_light.position.y / 100000, 1.0)
+	ambient_light.intensity = directional_light.intensity / 3
+	sky.material.uniforms["sunPosition"].value.copy(directional_light.position)
+	//--------------------------------------------------
 
 
 	renderer.render(scene, camera)
